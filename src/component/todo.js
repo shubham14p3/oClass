09a01f0ca44cdd1fccbd01cfb92ca925b0d00aac
Shubham Raj <shubham14p3@gmail.com>
2021-02-26 11:50:22 +0530

    added todo components

---
+++ src/component/todo.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import todo from "../images/todo.svg";
+import "./Todo.css";
+
+const Todo=()=>{
+return(
+    <>
+        <div className="main-div">
+            <div className="child-div">
+                <figure>
+                    <img src="" alt="todologo"></img>
+                </figure>
+            </div>
+        </div>
+    </>
+)
+}
+
+export default Todo;
